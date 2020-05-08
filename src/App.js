652d1fce311765a8d072b0986f262f1e3b76055c
shahinrajabinejad <rjmustafa77@gmail.com>
2020-05-09 02:15:33 +0430

    3 commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
-
+//var cookieParser = require('cookie-parser')
+//var logger = require('morgan');
 //Settings
 app.set('port', process.env.PORT || 3000);
-
 //Middlewares
+//app.use(logger('dev'));
 app.use(express.json());
+//app.use(cookieParser())
 // Configurar cabeceras y cors
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
@@ -14,6 +16,29 @@ app.use((req, res, next) => {
   res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
 });
+//auth
+app.use(function auth(req, res, next) {
+  console.log(req.headers);
+  var authHeader = req.headers.authorization;
+  if (!authHeader) {
+    var err = new Error('you are not Authenticated');
+    res.setHeader('www-Authenticate', 'Basic');
+    err.status = 401;
+    next(err);
+  }
+  var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
+  var username = auth[0];
+  var password = auth[1];
+  if (username === 'admin' && password === 'admin') {
+    next();
+  } else {
+    var err = new Error('you are not Authenticated');
+    res.setHeader('www-Authenticate', 'Basic');
+    err.status = 401;
+    next(err);
+  }
+});
+//app.use('auth');
 // importing routes
 const companyRouters = require('./routes/CompanyRoute')
 //Route
@@ -32,7 +57,6 @@ app.use('/test', (req, res) => {
 app.use('/', (req, res) => {
   res.send("Hello World form NodeJS express.");
 });
-
 
 app.listen(app.get('port'), () => {
   console.log("Start server on port " + app.get('port'))
